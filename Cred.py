4c53447101e674fc86dab212279d4775b48a35a9
Mezix Support <100026001+Mezix-Alyasa@users.noreply.github.com>
2022-10-21 09:24:46 +0300

    Created Cred.py

---
+++ Cred.py
@@ -0,0 +1,6 @@
+email="YOUR_EMAIL"
+password="PASSWORD" # given by google
+yourSheet="GOOGLE_SHEET_NAME"
+mainWorkSheetName="MAIN_DATA_SHEET"
+entryWorkSheetName="DAILY_ENTRY_SHEET"
+schoolName="SCHOOL_NAME"
