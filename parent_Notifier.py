defe22564330a5cdd26b238c9c2c820c86a681dd
Mezix Support <100026001+Mezix-Alyasa@users.noreply.github.com>
2022-10-21 09:21:42 +0300

    Now you have a seperate Cred File

--- parent_Notifier.py
+++ parent_Notifier.py
@@ -4,20 +4,20 @@ import time
 import smtplib
 import pandas as pd
 import gspread
+from Cred import *
 
 
 def Main():
     # Authenticating Sheets
-    # Calling the Service_Account File
     sa = gspread.service_account(filename="service_account.json")
-    sh = sa.open("Attendence")  # Name of the Google Sheet
-    wks = sh.worksheet("Master Data")  # Name of the Master Data WorkBook
-    wks2 = sh.worksheet("Daily Entry")  # Name of the Daily Entry WorkBook
+    sh = sa.open(yourSheet)
+    wks = sh.worksheet(mainWorkSheetName)
+    wks2 = sh.worksheet(entryWorkSheetName)
 
     # Importing and Cleaning The Data
     df_master = pd.DataFrame(wks.get_all_records(), index=None)
     df_daily_dirty = pd.DataFrame(wks2.get_all_records(), index=None)
-    df_daily = df_daily_dirty.drop_duplicates(subset=["Barcode"])
+    df_daily = df_daily_dirty.drop_duplicates(subset=["Barcode"]) 
 
     # Checking For Data Availability
     if(df_daily.empty == False):
@@ -25,8 +25,8 @@ def Main():
 
         #>>> Emailing Department <<<#
         # Credentials
-        your_email = "YOUR_EMAIL@GMAIL.COM"  # The sender's Email
-        your_password = "YOUR_APP_PASSWORD"  # The App Password
+        your_email = username
+        your_password = password
 
         # Establishing connection with gmail
         server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
@@ -40,14 +40,14 @@ def Main():
             parentname = df_data.Parent[row]
             email = df_data.Email[row]
             mailsent = df_data.Mailsent[row]
-            subject = f'{name} Have Reached School (IISJED)'
-            body = f'Hello {parentname}, {name} Has Reached School'
+            subject = f'{name} Have Reached School ({schoolName})'
+            body = f'Dear {parentname}, {name} Has Reached School'
             message = f'Subject: {subject},\n\n{body}'
 
             # Check If Email Already Sent
             if(mailsent == ""):
                 cell = wks2.find(str(barcode))
-                wks2.update_cell(cell.row, 2, "Y")
+                wks2.update_cell(cell.row, 2, "MAIL SENT")
                 print(f'Email Sent to {parentname} Saying ( {message} )')
                 server.sendmail(your_email, [email], message)
             else:
